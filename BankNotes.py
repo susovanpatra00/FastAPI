b259ae8588f4f3aa3ca7a0bcf23558de0a009074
susovanpatra00 <susovanpatra00@gmail.com>
2024-03-18 12:13:52 +0530

    Written ML Model

---
+++ BankNotes.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    variance: float 
+    skewness: float 
+    curtosis: float 
+    entropy: float
